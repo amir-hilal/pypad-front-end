8d614e242e52cd35166cd5907263397fdf7de35b
Abdallah <fafafavd@gmail.com>
2024-07-29 10:26:22 +0300

    clean code

--- src/components/CodeEditor/CodeEditor.jsx
+++ src/components/CodeEditor/CodeEditor.jsx
@@ -7,28 +7,20 @@ import { executeCode } from "../../services/CodeService";
 const CodeEditor = () => {
   const editorRef = useRef();
   const [value, setValue] = useState("");
-  const [language, setLanguage] = useState("javascript");
+  const [language, setLanguage] = useState("python");
 
-  // State variable to set users source code
   const [userCode, setUserCode] = useState(``);
 
-  // State variable to set editors default language
   const [userLang, setUserLang] = useState("python");
 
-  // State variable to set editors default theme
   const [userTheme, setUserTheme] = useState("vs-dark");
 
-  // State variable to set editors default font size
   const [fontSize, setFontSize] = useState(20);
 
-  // State variable to set users input
   const [userInput, setUserInput] = useState("");
 
-  // State variable to set users output
   const [userOutput, setUserOutput] = useState("");
 
-  // Loading state variable to show spinner
-  // while fetching data
   const [loading, setLoading] = useState(false);
 
   const options = {
@@ -51,7 +43,7 @@ const CodeEditor = () => {
     editor.focus();
   };
 
-  // Function to clear the output screen
+  //clear the output 
   function clearOutput() {
     setUserOutput("");
   }
