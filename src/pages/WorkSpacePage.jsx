f577ccead41201051e7819ba5e55836723d684e6
Abdallah <fafafavd@gmail.com>
2024-07-30 09:43:34 +0300

    Cleaned code

--- src/pages/WorkSpacePage.jsx
+++ src/pages/WorkSpacePage.jsx
@@ -4,10 +4,6 @@ import CodeEditor from '../components/CodeEditor/CodeEditor';
 
 const WorkspacePage = () => {
   return (
-    // <div className="bg-light h-75 text-light p-5 text-center">
-    //   <h1 className="text-neon">Workspace</h1>
-    //   <p>Start coding here!</p>
-    // </div>
     <CodeEditor/>
   );
 };
